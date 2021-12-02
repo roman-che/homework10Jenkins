80d57f86cb5084dc0736240cee1d7215395e56e4
roman <roman.chernyavsky@gmail.com>
2021-12-02 12:01:23 +0300

    added SystemPropertiesTests

---
+++ src/test/java/ru/inventorium/qa/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,53 @@
+package ru.inventorium.qa.tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+    @Test
+    void someTest() {
+        String value = System.getProperty("value");
+        System.out.println(value);
+        //null
+    }
+
+    @Test
+    void someTest1() {
+        String value = System.getProperty("value", "default_value");
+        System.out.println(value);
+        //default_value
+    }
+
+    @Test
+    void someTest2() {
+        System.setProperty("value", "another_value");
+        String value = System.getProperty("value", "default_value");
+        System.out.println(value);
+
+    }
+
+    @Test
+    @Tag("properties")
+    void someTest4() {
+        String value = System.getProperty("browser", "chrome");
+        System.out.println(value);
+
+    }
+
+    @Test
+    @Tag("properties")
+    void someTest5() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browser_size = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browser_size);
+
+        //запуск в терминале передаст параметры и заменит их значения
+        //gradle properties_test -Dbrowser=opera -Dversion=93
+    }
+
+
+}
